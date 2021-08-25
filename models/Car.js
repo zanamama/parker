cd1ceae25ef92fb447a0e8ba34d3eb2b8a031325
Burak Aksu <sabburak26@gmail.com>
2021-08-25 13:34:52 -0400

    created car model that has properties of brand model and color, with a foriegn key that refers to users

--- models/Car.js
+++ models/Car.js
@@ -3,3 +3,36 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Car extends Model {}
+
+Car.init(
+  {
+    brand: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      refernece: {
+        model: 'user',
+        key: 'id',
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelname: 'product',
+  }
+);
+
+module.exports = Car;
