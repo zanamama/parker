781d376b92bd1dc1fa7394f44517b6b2e75a3c76
Gospel Chukwu <brondchux@gmail.com>
2021-08-25 15:01:23 -0400

    Added middleware to allow use of static folder files

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// Static folder setup
+app.use(express.static(__dirname + "/public"));
+
 // generic fxn
 app.get("/", (req, res) => {
-  res.json("Welcome to Parker App!");
+	res.json("Welcome to Parker App!");
 });
 
 app.listen(PORT, (err) => {
-  if (err) {
-    console.log("Whoops, an error occured.", err);
-  } else {
-    console.log(`Got it! Listening on PORT ${PORT}`);
-  }
+	if (err) {
+		console.log("Whoops, an error occured.", err);
+	} else {
+		console.log(`Got it! Listening on PORT ${PORT}`);
+	}
 });
