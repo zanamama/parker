839f5322dfbe8f22f37b9a25bb31f83e687b766d
Burak Aksu <sabburak26@gmail.com>
2021-08-25 15:11:52 -0400

    small change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// generic fxn
+// generic fxn!
 app.get("/", (req, res) => {
   res.json("Welcome to Parker App!");
 });
