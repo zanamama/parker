152c869db32b7b55a9b15255f09542c8316dda04
Gospel Chukwu <brondchux@gmail.com>
2021-08-25 18:20:54 -0400

    Added the route endpoints for Cars, Users and Locations

--- controllers/api/car-routes.js
+++ controllers/api/car-routes.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 
 // GET /api/cars
 router.get("/", (req, res) => {
+	// TODO: add function to display all cars here and include all foreign properties
 	res.status(200).json({ method: req.method, endpoint: "/api/cars" });
 });
 
 // GET /api/cars/:id
 router.get("/:id", (req, res) => {
+	// TODO: add fetching of individual cars function here using id
 	res
 		.status(200)
 		.json({ method: req.method, endpoint: `/api/cars/${req.params.id}` });
@@ -14,10 +16,33 @@ router.get("/:id", (req, res) => {
 
 // POST /api/cars
 router.post("/", (req, res) => {
+	// TODO: add create car function here
+
 	res.status(200).json({
 		method: req.method,
 		endpoint: `/api/cars`,
 		...req.body,
+	});
+});
+
+// PUT /api/cars/:id
+router.put("/:id", (req, res) => {
+	// TODO: add update car function here
+
+	res.status(200).json({
+		method: req.method,
+		endpoint: `/api/cars/${req.params.id}`,
+		...req.body,
+	});
+});
+
+// DELETE /api/cars/:id
+router.delete("/:id", (req, res) => {
+	// TODO: add update car function here
+
+	res.status(200).json({
+		method: req.method,
+		endpoint: `/api/cars/${req.params.id}`,
 	});
 });
 
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const carRoutes = require("./car-routes");
+const userRoutes = require("./user-routes");
+const locationRoutes = require("./location-routes");
 
 router.use("/cars", carRoutes);
+router.use("/users", userRoutes);
+router.use("/locations", locationRoutes);
 
 module.exports = router;
