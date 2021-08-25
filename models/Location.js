096676d2486de9f55552acb69fdc407b41a1a087
Burak Aksu <sabburak26@gmail.com>
2021-08-25 13:45:36 -0400

    created a location model that has properties address and isOpen

--- models/Location.js
+++ models/Location.js
@@ -3,3 +3,26 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Location extends Model {}
+
+Location.init(
+  {
+    adress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isOpen: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'location',
+  }
+);
+
+module.exports = Location;
