05b8eac00519a1bffc3102d797b3ea5d7042183c
Hassan Yusuf <hassanyousuf1999@gmail.com>
2021-08-26 11:34:07 -0400

    Cars seed created and also updated locationSeed where replaced isOpen to numOpenSpace.

--- seeds/cars.js
+++
@@ -1,2 +0,0 @@
-// TODO: Rename seed files to folow this format: example users.js => userSeeds.js
-// TODO: Add car seeds here
---
+++ seeds/carsSeed.js
@@ -0,0 +1,40 @@
+const { Car } = require("../models");
+
+const cars = [
+	{
+		brand: "Honda",
+		model: "Civic",
+		color: "White",
+		user_id: 1,
+        location_id: 2
+	},
+
+    {
+		brand: "Nissan",
+		model: "Maxima",
+		color: "Black",
+		user_id: 3,
+        location_id: 1
+	},
+
+    {
+		brand: "BMW",
+		model: "X1",
+		color: "Red",
+		user_id: 2,
+        location_id: 4
+	},
+
+    {
+		brand: "Toyota",
+		model: "Highlander",
+		color: "gray",
+		user_id: 5,
+        location_id: 3
+	},
+	
+];
+
+const carsData = () => Car.bulkCreate(cars);
+
+module.exports = carsData;
