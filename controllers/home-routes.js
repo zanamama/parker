f4634bae51e97c29f6f11e8f55fc11c9ac9ba478
Burak Aksu <sabburak26@gmail.com>
2021-08-26 20:34:18 -0400

    created register handlebars page, also modified the login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -15,6 +15,10 @@ router.get("/login", (req, res) => {
 	res.render("login");
 });
 
+router.get("/register", (req, res) => {
+	res.render("register");
+});
+
 router.get("/profile", (req, res) => {
 	res.render("profile");
 });
