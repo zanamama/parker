9b331d17ce97d3137f089887e5547020c2c8865c
zanama <57731386+zanmama@users.noreply.github.com>
2021-08-28 18:19:35 -0400

    home-routes.js and login.js in sync to store email and apw from user login page

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -7,11 +7,24 @@ document
 
     const password = document.getElementById("password").value.trim();
 
+    const message = document.getElementById("login-message");
+
     if (email && password) {
       const response = await fetch("/login", {
         method: "POST",
         body: JSON.stringify({ email, password }),
         headers: { "Content-Type": "application/json" },
       });
+
+      if (response.ok) {
+        document.location.replace("/");
+      } else {
+        message.innerText = "Incorrect Email or Password";
+        message.style.color = "red";
+        setTimeout(() => {
+          message.innerText = "Must be 8+ Characters in Length";
+          message.style.color = "black";
+        }, 2000);
+      }
     }
   });
